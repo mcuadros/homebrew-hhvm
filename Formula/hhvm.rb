94dbd604b90d157a796d6e6d9f2f70cea0f8567f
Maurus Cuelenaere <mcuelenaere@gmail.com>
2014-02-09 13:16:54 +0100

    Install hhvm in the Cellar

--- Formula/hhvm.rb
+++ Formula/hhvm.rb
@@ -89,7 +89,8 @@ class Hhvm < Formula
       "-DLIBINTL_INCLUDE_DIR=#{Formula.factory('gettext').opt_prefix}/include",
       "-DLIBDWARF_LIBRARIES=#{Formula.factory('libdwarf').opt_prefix}/lib/libdwarf.3.dylib",
       "-DLIBDWARF_INCLUDE_DIRS=#{Formula.factory('libdwarf').opt_prefix}/include",
-      "-DFREETYPE_INCLUDE_DIRS=#{Formula.factory('freetype').opt_prefix}/include/freetype2/"
+      "-DFREETYPE_INCLUDE_DIRS=#{Formula.factory('freetype').opt_prefix}/include/freetype2/",
+      "-DCMAKE_INSTALL_PREFIX=#{prefix}"
     ]
 
     ENV['HPHP_HOME'] = Dir.pwd
