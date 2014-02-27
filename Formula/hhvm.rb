bebb6e5824b0e37b6c109c632182081f9fb32028
Ben Davies <ben.davies@boxuk.com>
2014-02-27 16:07:30 +0000

    Add missing commas

--- Formula/hhvm.rb
+++ Formula/hhvm.rb
@@ -72,8 +72,8 @@ class Hhvm < Formula
       "-DCMAKE_LIBRARY_PATH=\"#{HOMEBREW_PREFIX}/lib:/usr/lib\"",
       "-DLIBEVENT_LIB=#{Formula.factory('libeventfb').opt_prefix}/lib/libevent.dylib",
       "-DLIBEVENT_INCLUDE_DIR=#{Formula.factory('libeventfb').opt_prefix}/include",
-      "-DLIBMAGICKWAND_INCLUDE_DIRS=#{Formula.factory('imagemagick').opt_prefix}/include/ImageMagick-6"
-      "-DLIBMAGICKWAND_LIBRARIES=#{Formula.factory('imagemagick').opt_prefix}/lib/libMagickWand-6.Q16.dylib"
+      "-DLIBMAGICKWAND_INCLUDE_DIRS=#{Formula.factory('imagemagick').opt_prefix}/include/ImageMagick-6",
+      "-DLIBMAGICKWAND_LIBRARIES=#{Formula.factory('imagemagick').opt_prefix}/lib/libMagickWand-6.Q16.dylib",
       "-DICU_INCLUDE_DIR=#{Formula.factory('icu4c').opt_prefix}/include",
       "-DICU_LIBRARY=#{Formula.factory('icu4c').opt_prefix}/lib/libicuuc.dylib",
       "-DICU_I18N_LIBRARY=#{Formula.factory('icu4c').opt_prefix}/lib/libicui18n.dylib",
